ad77f19eb353f5497f057931732bb9d675bd4cf8
yassirelkhaili <elkhailiyassir@gmail.com>
2024-03-25 10:43:45 +0000

    basic project structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Home
